b7c547d5cbd4da64080afdb7eabdf0fbb27556fc
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2020-05-31 17:05:49 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express PBL – As simple as possible demo of embed capabilities
+
+### References 
+https://github.com/bryan-at-looker/embed-api-sdk-needs-cors-proxy
--- routes/webhooks.js
+++ routes/webhooks.js
@@ -4,16 +4,12 @@ var router = express.Router();
 var looker = require('@looker/sdk');
 var sdk = looker.LookerNodeSDK.init31(new looker.NodeSettings())
 
-var config = require('../config.js');
-
 router.get('/me', function(req, res) {
-  console.log('webhooks.js: Me Pressed')
   sdk.me()
     .then( (result) => res.json(result) )
 })
 
 router.get('/models', function(req, res) {
-  console.log('webhooks.js: Models Pressed')
   sdk.all_lookml_models('name')
     .then( (result) => res.json(result) )
 })
